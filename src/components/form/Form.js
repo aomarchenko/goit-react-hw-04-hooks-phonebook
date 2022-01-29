eb7fb1cc4d97cd947b046fe19cde5d6cf00759c1
Олександр Марченко <80917411+aomarchenko@users.noreply.github.com>
2022-01-29 21:52:59 +0200

    done

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,7 +5,6 @@ import styles from './Form.module.css';
 export default function Form(props) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
-  const [contacts, setContacts] = useState([]);
 
   const inputId = shortid.generate();
 
@@ -29,7 +28,6 @@ export default function Form(props) {
   const reset = () => {
     setNumber('');
     setName('');
-    setContacts([]);
   };
 
   return (
@@ -71,80 +69,3 @@ export default function Form(props) {
     </>
   );
 }
-
-// import React, { Component } from 'react';
-// import shortid from 'shortid';
-// import styles from './Form.module.css';
-
-// class Form extends Component {
-//   state = {
-//     contacts: [],
-//     name: '',
-//     number: '',
-//   };
-
-//   inputId = shortid.generate();
-
-//   handleInputChange = event => {
-//     this.setState({ [event.currentTarget.name]: event.currentTarget.value });
-//   };
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-
-//     this.props.onSubmit(this.state.name, this.state.number);
-
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({
-//       contacts: [],
-//       name: '',
-//       number: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <h1 className={styles.formTitle}>Phonebook</h1>
-//         <form className={styles.form} onSubmit={this.handleSubmit}>
-//           <label className={styles.label} htmlFor={this.inputId}>
-//             Name
-//             <input
-//               className={styles.input}
-//               id={this.inputId}
-//               type="text"
-//               name="name"
-//               value={this.state.name}
-//               onChange={this.handleInputChange}
-//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//               required
-//             />
-//           </label>
-//           <label className={styles.label} htmlFor={this.inputId}>
-//             Number
-//             <input
-//               className={styles.input}
-//               id={this.inputId}
-//               value={this.state.number}
-//               onChange={this.handleInputChange}
-//               type="tel"
-//               name="number"
-//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//               required
-//             />
-//           </label>
-//           <button className={styles.button} type="submit">
-//             Add contact
-//           </button>
-//         </form>
-//       </>
-//     );
-//   }
-// }
-
-// export default Form;
